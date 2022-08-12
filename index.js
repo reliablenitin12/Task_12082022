c0f04389269f721ef12451c279b194cc69122fbe
Nitin <nk345264@gmail.com>
2022-08-12 10:43:38 +0530

    set up the directory structure

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port=8000;
 const app=express();
 
 
+//telling app to use ejs view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
 if(err){
